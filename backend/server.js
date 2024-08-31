77d7470d6062b683669626d91d92c89f6bb0bd94
Vijayshankar <vijayshankar871992@gmail.com>
2024-08-31 16:52:23 +0530

    Deployment WIP

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,10 @@ app.use('/api/reviews', reviewRoutes);
 app.use('/api/purchases', purchaseRoutes);  
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 app.listen(PORT, () => {
